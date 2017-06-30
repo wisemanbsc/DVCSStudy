654bfc56351c31f02d7779e1d92337eba9bbe146
USER <USER@example.com>
2017-06-30 11:26:51 +0900

    aa

---
+++ Github/src/Github.java
@@ -0,0 +1,9 @@
+
+public class Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Github/src/Github1.java
@@ -0,0 +1,10 @@
+
+public class Github1 {
+	public static int add(int num1, int num2) {
+		return num1 * num2;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
