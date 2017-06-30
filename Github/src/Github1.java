ed1543c59c597bcc49b4e95734931c95ef5ddfe7
USER <USER@example.com>
2017-06-30 11:30:04 +0900

    commit02

--- Github/src/Github1.java
+++ Github/src/Github1.java
@@ -4,6 +4,10 @@ public class Github1 {
 		return num1 * num2;
 	}
 	
+	public static int division(int num1, int num2) {
+		return num1 / num2;
+	}
+	
 	public static void main(String[] args) {
 		
 	}
